50ec29f21f168c9c9076768bd86af992291f7e51
Your Name <you@example.com>
2020-12-18 15:49:33 -0500

    [.env] Added

---
+++ src/components/Signup.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+        </div>
+    )
+}
+
+export default Signup;
